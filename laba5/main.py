9d6c8c46b2754555961e0d1fc447512562fb78b9
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-12-09 14:02:10 +1000

    Update main.py

--- laba5/main.py
+++ laba5/main.py
@@ -1,8 +1,6 @@
 import socket
 import struct
-
-
-ICMP_ECHO_REQUEST = 8
+import time
 
 
 def checksum(source_string):
@@ -10,12 +8,12 @@ def checksum(source_string):
     countTo = (len(source_string) // 2) * 2
     count = 0
     while count < countTo:
-        thisVal = ord(source_string[count + 1]) * 256 + ord(source_string[count])
+        thisVal = source_string[count + 1] * 256 + source_string[count]
         sum = sum + thisVal
         sum = sum & 0xffffffff
         count = count + 2
     if countTo < len(source_string):
-        sum = sum + ord(source_string[len(source_string) - 1])
+        sum = sum + source_string[len(source_string) - 1]
         sum = sum & 0xffffffff
     sum = (sum >> 16) + (sum & 0xffff)
     sum = sum + (sum >> 16)
@@ -27,3 +25,10 @@ def checksum(source_string):
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    data = bytes([0x40]*55)
+    header = struct.pack("!BBHHH", 8, 0, 0, 4242, 0)
+    header = struct.pack("!BBHHH", 8, 0, checksum(header+data), 64242, 0)
+    packet = header + data
+
+    s.sendto(packet, ('127.0.0.1', 0))
+    time.sleep(16)

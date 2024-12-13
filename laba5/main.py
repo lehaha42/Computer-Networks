b8e076edb2a21f8956e3924aa60c557c8b86500d
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-12-13 18:50:44 +1000

    done

--- laba5/main.py
+++ laba5/main.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 import time
+import os
 
 
 def checksum(source_string):
@@ -23,12 +24,42 @@ def checksum(source_string):
     return answer
 
 
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    data = bytes([0x40]*55)
-    header = struct.pack("!BBHHH", 8, 0, 0, 4242, 0)
-    header = struct.pack("!BBHHH", 8, 0, checksum(header+data), 64242, 0)
-    packet = header + data
+def create_packet():
+    data = bytes([0x42] * 42)
+    id = os.getpid() & 0xFFFF
+    header = struct.pack("!BBHHH", 8, 0, 0, id, 0)
+    header = struct.pack("!BBHHH", 8, 0, checksum(header + data), id, 0)
+    return header + data
 
-    s.sendto(packet, ('127.0.0.1', 0))
-    time.sleep(16)
+
+def traceroute(ip, max_ttl, timeout):
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    packet = create_packet()
+    s.settimeout(timeout)
+    for i in range(max_ttl):
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, i+1)
+        s.sendto(packet, (ip, 0))
+        try:
+            start = time.time()
+            res, ret_ip = s.recvfrom(4096)
+            dif = (time.time() - start)*1000.
+            print(f'{i+1}\t{dif:.2f}ms\t{ret_ip[0]}')
+            if ret_ip[0] == ip:
+                break
+        except TimeoutError:
+            print(f'{i+1}\t  *   \ttime out')
+
+
+def main():
+    inp = input(': ').split(' ')
+    max_ttl = 30
+    timeout = 3
+    if len(inp) > 2:
+        timeout = int(inp[2])
+    if len(inp) > 1:
+        max_ttl = int(inp[1])
+    traceroute(inp[0], max_ttl, timeout)
+
+
+if __name__ == '__main__':
+    main()
